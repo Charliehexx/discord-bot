f4777a5b70ebfb94bbc06875f2087bd56f82a5c0
Charliehexx <adi2652002@gmail.com>
2022-04-05 18:34:18 +0530

    Initial Commit

---
+++ test/functions/events/discord/message/create/prefix/adi.js
@@ -0,0 +1,8 @@
+// authenticates you with the API standard library
+// type `await lib.` to display API autocomplete
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+await lib.discord.channels['@0.3.1'].messages.create({
+  channel_id: `${context.params.event.channel_id}`,
+  content: `https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-chevrolet-corvette-z06-1607016574.jpg?crop=0.737xw:0.738xh;0.181xw,0.218xh&resize=640:*`
+});
